2f76a9bef32638ac477490586b894ab221472c87
Christopher Braun <christopher.braun@stud.h-da.de>
2020-10-07 18:36:57 +0200

    some tinkering with image functions

--- read_image.py
+++ read_image.py
@@ -1,4 +1,12 @@
 from PIL import Image
+import os
+
+os.chdir("Bachelorarbeit/")
+
 jpgfile = Image.open("picture.jpg")
 
 print(jpgfile.bits, jpgfile.size, jpgfile.format)
+
+print(jpgfile.getexif())
+
+print(len(list(jpgfile.getdata())))
