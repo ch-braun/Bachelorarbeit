ab723e621f09c72461e83bfea81d13289f2baee5
Christopher Braun <christopher.braun@stud.h-da.de>
2020-10-09 15:11:41 +0200

    Made resizing and saving the output functional

--- read_image.py
+++ read_image.py
@@ -6,14 +6,24 @@ os.chdir("Bachelorarbeit/")
 
 jpgfile = Image.open("picture.jpg")
 
-print(jpgfile.bits, jpgfile.size, jpgfile.format)
-
 print(jpgfile.getexif())
 
-before = time.time()
+(width, height) = (jpgfile.width, jpgfile.height)
 
-print(len(list(jpgfile.getdata())))
+for i in range(4):
 
-after = time.time()
+    print("--------------------------- Iteration:", i, "---------------------------")
 
-print(after - before, "s")
+    (width, height) = (width // 2, height // 2)
+
+    jpgfile.thumbnail((width, height), Image.BILINEAR)
+
+    print(jpgfile.bits, jpgfile.size, jpgfile.format)
+
+    before = time.time()
+
+    print(len(list(jpgfile.getdata())))
+
+    after = time.time()
+
+jpgfile.save("new_pic.jpg", "JPEG")
