e87908ed143f0d7ec5132f261faf734263df7415
christopher.braun <christopher.braun@cbit.solutions>
2020-12-17 15:17:06 +0100

    Added dataset stat generation

--- src/xml_preprocessing.py
+++ src/xml_preprocessing.py
@@ -1,14 +1,18 @@
+from datetime import datetime
 import os
 from pathlib import Path
+import re
 import xml.etree.ElementTree as elemTree
 
 DATA_DIR = "data/"
 SPLIT_DIR = DATA_DIR + "split/"
+STAT_DIR = DATA_DIR + "stat/"
 
 
 def split_xml_files() -> None:
     print("Splitting XML-files...")
     Path(SPLIT_DIR).mkdir(parents=True, exist_ok=True)
+    Path(STAT_DIR).mkdir(parents=True, exist_ok=True)
 
     for filename in os.listdir(DATA_DIR):
         if filename.lower().endswith(".xml"):
@@ -29,19 +33,44 @@ def split_xml_files() -> None:
             old_file.close()
 
 
-def parse_xml_files() -> None:
-    print("Parsing XML-files...")
+def parse_xml_file(filename) -> elemTree.Element:
+    tree = elemTree.parse(filename)
+    return tree.getroot()
+
+
+def create_dataset_stats() -> None:
+    print("Creating dataset statistics...")
+    tables = dict()
+    stat_file = "stat_" + datetime.now().strftime("%Y-%m-%d (%H:%M:%S)") + ".csv"
     for filename in os.listdir(SPLIT_DIR):
         if filename.lower().endswith(".xml"):
-            tree = elemTree.parse(SPLIT_DIR + filename)
-            root = tree.getroot()
-            for child in root.iter("V_PARTNER"):
-                print(filename, "|", child.tag, child.text)
-                break
+            root = parse_xml_file(SPLIT_DIR + filename)
+            heap = None
+            file_tables = dict()
+            for elem in root:
+                elem.tag = re.sub(r'{.*}', '', elem.tag.lower())
+                if elem.tag == 'heap':
+                    heap = elem
+                    break
 
-            for child in root.iter("SCORE1"):
-                print(filename, "|", child.tag, child.text)
+            if heap is not None:
+                for elem in heap:
+                    elem.tag = re.sub(r'{.*}', '', elem.tag.lower())
+                    print(filename, elem.tag)
+                    if elem.tag.lower() not in file_tables.keys():
+                        file_tables[elem.tag.lower()] = 1
+                        if elem.tag.lower() in tables.keys():
+                            tables[elem.tag.lower()] += 1
+                        else:
+                            tables[elem.tag.lower()] = 1
+
+    header = ';'.join(map(str, tables.keys()))
+    content = ';'.join(map(str, tables.values()))
+    stat_file = open(STAT_DIR + stat_file, "w", encoding="utf-8")
+    stat_file.write(header + "\n")
+    stat_file.write(content + "\n")
+    stat_file.close()
 
 
 split_xml_files()
-# parse_xml_files()
+create_dataset_stats()
