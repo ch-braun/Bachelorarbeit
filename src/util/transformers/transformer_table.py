b876513860f87d19102c5890feb1afe8e8581030
christopher.braun <christopher.braun@cbit.solutions>
2021-01-23 12:35:39 +0100

    Removed unused tables and their corresponding transformers

--- src/util/transformers/transformer_table.py
+++ src/util/transformers/transformer_table.py
@@ -11,16 +11,6 @@ class TransformerZdkk_rb_scores(Transformer):
         pass
 
 
-class TransformerZdkk_sb_gp_azam(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerEkun(Transformer):
-    def transform(self):
-        pass
-
-
 class TransformerDfkkko(Transformer):
     def transform(self):
         pass
@@ -46,57 +36,12 @@ class TransformerFkkmako(Transformer):
         pass
 
 
-class TransformerTiban(Transformer):
-    def transform(self):
-        pass
-
-
 class TransformerDfkklocks(Transformer):
     def transform(self):
         pass
 
 
 class TransformerZdkk_mw_vtref_st(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDfkkcohi(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDfkklocksh(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDfkkcoh(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_mwk_xml_snd(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_mwk_xml_dok(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_mwk_xml_cor(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_mwk_xml_pos(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_abg_vors(Transformer):
     def transform(self):
         pass
 
@@ -111,72 +56,7 @@ class TransformerDfkkzp(Transformer):
         pass
 
 
-class TransformerSepa_mandate(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDfkkwoh(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_rb_ausb(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_ba_fv_ausb(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_abg_ausb_po(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_abg_ink(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_rb_ereignis(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_mw_ribs_cmo(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDfkkrapt(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDfkkrat(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_rechb1_bas(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_im_inkstat(Transformer):
-    def transform(self):
-        pass
-
-
 class TransformerDpayh(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDpayp(Transformer):
     def transform(self):
         pass
 
@@ -186,62 +66,12 @@ class TransformerFkk_instpln_head(Transformer):
         pass
 
 
-class TransformerFkk_instpln_hist(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_sb_vv_vt(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_mw_dkb_list(Transformer):
-    def transform(self):
-        pass
-
-
 class TransformerDfkkrk(Transformer):
     def transform(self):
         pass
 
 
-class TransformerDfkkrp(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_dk_rk_rp(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDfkkrh(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_rp_typ_map(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerSepa_mandate_use(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_mw_mhn_ads(Transformer):
-    def transform(self):
-        pass
-
-
 class TransformerZdkk_bapi_obcfc(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerFkkmakt(Transformer):
     def transform(self):
         pass
 
@@ -256,37 +86,12 @@ class TransformerZdkk_zv_azahl(Transformer):
         pass
 
 
-class TransformerZdkk_debdel(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_dfkkdda(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_instplnhis2(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_instplnhist(Transformer):
-    def transform(self):
-        pass
-
-
 class TransformerZdkk_crpo(Transformer):
     def transform(self):
         pass
 
 
 class TransformerZdkk_dk_san_bas(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDfkkcfzst(Transformer):
     def transform(self):
         pass
 
@@ -304,24 +109,3 @@ class TransformerFkk_sec(Transformer):
 class TransformerFkk_sec_c(Transformer):
     def transform(self):
         pass
-
-
-class TransformerZdkk_mw_vtref_sh(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDfkkzpe(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerDfkkcrh(Transformer):
-    def transform(self):
-        pass
-
-
-class TransformerZdkk_rb_st_vkont(Transformer):
-    def transform(self):
-        pass
-
