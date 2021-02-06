abab1588775159a39bb60c11870fd8eadf8d359f
christopher.braun <christopher.braun@cbit.solutions>
2021-02-06 12:50:39 +0100

    Added data plotting and dynamic oversampling

--- src/step04_classification.py
+++ src/step04_classification.py
@@ -5,11 +5,14 @@ from datetime import datetime
 from pathlib import Path
 
 import numpy as np
+from matplotlib import pyplot
+from plot_keras_history import plot_history
 from tensorflow import metrics
 from tensorflow.keras import layers
 from tensorflow.python.keras import Model, activations
 from tensorflow.python.keras.backend import clear_session
 from tensorflow.python.keras.utils.np_utils import to_categorical
+from tensorflow.python.keras.utils.vis_utils import plot_model
 
 from step01_data_collection import STAT_DIR, DATA_DIR
 from step03_data_preprocessing import INDEPENDENT_DIR
@@ -67,7 +70,7 @@ def load_data():
     print('Label data types:', globals()['LABELS'].dtype)
 
 
-def split_data():
+def split_data(oversampling_factor=1):
     counts = dict()
     for i in range(0, len(globals()['LABELS'])):
         label = str(globals()['LABELS'][i])
@@ -87,7 +90,7 @@ def split_data():
         label_vect[label] = 1
         if counts[label] > 0:
             if 1 < int(label) < 9:
-                for x in range(0, 10):
+                for x in range(0, oversampling_factor):
                     globals()['VALIDATION_DATA'].append(entity)
                     globals()['VALIDATION_LABELS'].append(label)
             else:
@@ -96,7 +99,7 @@ def split_data():
             counts[label] -= 1
         else:
             if 1 < int(label) < 9:
-                for x in range(0, 10):
+                for x in range(0, oversampling_factor):
                     globals()['TRAINING_DATA'].append(entity)
                     globals()['TRAINING_LABELS'].append(label)
             else:
@@ -133,16 +136,21 @@ def create_neural_network(hidden_neurons: int) -> Model:
     return model
 
 
-def train_model(model: Model):
-    model.compile(optimizer='rmsprop',
+def train_model(model: Model) -> dict:
+    model.compile(optimizer='adam',
                   loss='mean_squared_error',
                   metrics=[metrics.CategoricalAccuracy(), metrics.Recall(), metrics.Precision()])
 
     y_train = to_categorical(globals()['TRAINING_LABELS'])
+    y_validate = to_categorical(globals()['VALIDATION_LABELS'])
     print(y_train.shape)
 
-    history = model.fit(x=globals()['TRAINING_DATA'], y=y_train, batch_size=32, epochs=10)
-    print(history.history)
+    history = model.fit(x=globals()['TRAINING_DATA'], y=y_train, batch_size=32, epochs=10,
+                        validation_data=(globals()['VALIDATION_DATA'], y_validate),
+                        validation_batch_size=32,
+                        validation_freq=1)
+
+    return history.history
 
 
 def validate_model(model: Model) -> [float, float, float, float]:
@@ -159,28 +167,29 @@ def validate_model(model: Model) -> [float, float, float, float]:
     return [loss, acc, recall, precision]
 
 
-def validate_model_for_rare_classes(model: Model) -> [float, float, float, float]:
-    rare_labels = list()
-    rare_entities = list()
+def validate_model_for_rare_classes(model: Model) -> dict:
+    rare_labels = dict()
+    rare_entities = dict()
+    results = dict()
     for i in range(0, len(globals()['VALIDATION_LABELS'])):
         if int(globals()['VALIDATION_LABELS'][i]) > 1:
-            rare_labels.append(globals()['VALIDATION_LABELS'][i])
-            rare_entities.append(globals()['VALIDATION_DATA'][i])
+            if globals()['VALIDATION_LABELS'][i] not in rare_labels.keys():
+                rare_labels[globals()['VALIDATION_LABELS'][i]] = list()
+                rare_entities[globals()['VALIDATION_LABELS'][i]] = list()
+            rare_labels[globals()['VALIDATION_LABELS'][i]].append(globals()['VALIDATION_LABELS'][i])
+            rare_entities[globals()['VALIDATION_LABELS'][i]].append(globals()['VALIDATION_DATA'][i])
 
-    rare_labels = np.asarray(rare_labels)
-    rare_entities = np.asarray(rare_entities)
+    for key in rare_labels.keys():
+        label = np.asarray(rare_labels[key] + ['9'])
+        entities = np.asarray(rare_entities[key])
 
-    y_validate = to_categorical(rare_labels)
-    print(y_validate.shape)
+        y_validate = to_categorical(label)[:-1]
+        print(y_validate.shape)
 
-    loss, acc, recall, precision = model.evaluate(x=rare_entities, y=y_validate, batch_size=32)
+        loss, acc, recall, precision = model.evaluate(x=entities, y=y_validate, batch_size=32)
+        results[key] = [loss, acc, recall, precision]
 
-    print("loss: %.4f" % loss)
-    print("categorical accuracy: %.4f" % acc)
-    print("recall: %.4f" % recall)
-    print("precision: %.4f" % precision)
-
-    return [loss, acc, recall, precision]
+    return results
 
 
 def calculate_models():
@@ -197,14 +206,34 @@ def calculate_models():
     min_value = to_categorical(globals()['VALIDATION_LABELS']).shape[1]
     max_value = len(globals()['DATA'][0])+1
 
+    model = None
     for x in range(min_value, max_value):
         print("Training NN for " + str(x) + " hidden neurons...")
         clear_session()
         model = create_neural_network(x)
-        train_model(model)
+        history = train_model(model)
         model.save(model_folder + "model_" + str(x))
+
+        plot_history(history, path=model_folder + "model_" + str(x) + "/history.png")
+        pyplot.close()
+
         results.append([x] + validate_model(model))
-        rare_results.append([x] + validate_model_for_rare_classes(model))
+        temp = validate_model_for_rare_classes(model)
+        to_append = [x]
+        for key in sorted(temp.keys()):
+            to_append += temp[key]
+        rare_results.append(to_append)
+
+    plot_model(
+        model,
+        to_file=model_folder + "/model.png",
+        show_shapes=False,
+        show_dtype=False,
+        show_layer_names=True,
+        rankdir="TB",
+        expand_nested=False,
+        dpi=100,
+    )
 
     output_file = STAT_DIR + "models_" + timestamp + '.csv'
     output_file = open(output_file, "w", encoding="utf-8")
@@ -215,7 +244,9 @@ def calculate_models():
 
     rare_output_file = STAT_DIR + "models_rare_" + timestamp + '.csv'
     rare_output_file = open(rare_output_file, "w", encoding="utf-8")
-    rare_output_file.write('neurons;loss;accuracy;recall;precision\n')
+    rare_output_file.write('neurons;loss2;accuracy2;recall2;precision2;loss4;accuracy4;recall4;precision4;' +
+                           'loss6;accuracy6;recall6;precision6;loss7;accuracy7;recall7;precision7;' +
+                           'loss8;accuracy8;recall8;precision8;loss9;accuracy9;recall9;precision9\n')
     for r in rare_results:
         rare_output_file.write(";".join(list(map(str, r))) + "\n")
     rare_output_file.close()
@@ -225,5 +256,8 @@ def calculate_models():
 def do_step(args: argparse.Namespace) -> None:
     if not args.skip_classification:
         load_data()
-        split_data()
+        if args.oversample:
+            split_data(args.oversample)
+        else:
+            split_data()
         calculate_models()
